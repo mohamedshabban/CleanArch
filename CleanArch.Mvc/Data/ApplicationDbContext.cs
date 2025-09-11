3e0d78e9248692b9ec75a5dcb1ee497b4db91174
Mohamed Shaban <mohamedshabban707@gmail.com>
2025-09-11 22:31:05 +0300

    Add project files.

---
+++ CleanArch.Mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Mvc.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "UniversityDbConnection",
+                    x => x.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)
+                          .MigrationsHistoryTable("__EFMigrationsHistory", "dbo"));
+            }
+        }
+    }
+}
